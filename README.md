8802f09be769ac4a9e261584d3c78f800c0f540f
Lenny Shleymovich <lenny@shleymovich.com>
2019-12-30 19:18:58 -0600

    Applied public files from 2019-12-30

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # OpenNN
+
 OpenNN is a software library written in C++ for advanced analytics. It implements neural networks, the most successful machine learning method. 
 
-The main advantage of OpenNN is its high performance. This library outstands in terms of execution speed and memory allocation. It is constantly optimized and parallelized in order to maximize its efficiency.
+The main advantage of OpenNN is its high performance. 
+
+This library outstands in terms of execution speed and memory allocation. It is constantly optimized and parallelized in order to maximize its efficiency.
 
 Some typical applications of OpenNN are business intelligence (customer segmentation, churn prevention...), health care (early diagnosis, microarray analysis...) and engineering (performance optimization, predictive maitenance...).
 
-OpenNN does not deal with computer vision or natural language processing.
+The documentation is composed by tutorials and examples to offer a complete overview about the library. 
 
-The documentation is composed by tutorials and examples to offer a complete overview about the library. The documentation can be found at the official <a href="http://opennn.net" target="_blank">OpenNN site</a>.
+The documentation can be found at the official <a href="http://opennn.net" target="_blank">OpenNN site</a>.
 
 CMakeLists.txt are build files for CMake, it is also used by the CLion IDE.
 
--- opennn/neural_network.h
+++ opennn/neural_network.h
@@ -1,17 +1,13 @@
-/****************************************************************************************************************/
-/*                                                                                                              */
-/*   OpenNN: Open Neural Networks Library                                                                       */
-/*   www.opennn.net                                                                                             */
-/*                                                                                                              */
-/*   N E U R A L   N E T W O R K   C L A S S   H E A D E R                                                      */
-/*                                                                                                              */
-/*   Artificial Intelligence Techniques SL                                                                      */
-/*   artelnics@artelnics.com                                                                                    */
-/*                                                                                                              */
-/****************************************************************************************************************/
+//   OpenNN: Open Neural Networks Library
+//   www.opennn.net
+//
+//   N E U R A L   N E T W O R K   C L A S S   H E A D E R                 
+//
+//   Artificial Intelligence Techniques SL
+//   artelnics@artelnics.com
 
-#ifndef __NEURALNETWORK_H__
-#define __NEURALNETWORK_H__
+#ifndef NEURALNETWORK_H
+#define NEURALNETWORK_H
 
 // System includes
 
@@ -23,10 +19,6 @@
 #include <sstream>
 #include <errno.h>
 
-#ifdef __OPENNN_MPI__
-#include <mpi.h>
-#endif
-
 // OpenNN includes
 
 #include "vector.h"
@@ -34,19 +26,17 @@
 
 #include "data_set.h"
 
+#include "layer.h"
 #include "perceptron_layer.h"
-#include "multilayer_perceptron.h"
-#include "inputs_trending_layer.h"
 #include "scaling_layer.h"
 #include "principal_components_layer.h"
 #include "unscaling_layer.h"
-#include "outputs_trending_layer.h"
 #include "bounding_layer.h"
 #include "probabilistic_layer.h"
-#include "inputs.h"
-#include "outputs.h"
-
-// TinyXml includes
+#include "convolutional_layer.h"
+#include "pooling_layer.h"
+#include "long_short_term_memory_layer.h"
+#include "recurrent_layer.h"
 
 #include "tinyxml2.h"
 
@@ -54,172 +44,111 @@ namespace OpenNN
 {
 
 /// This class represents the concept of neural network in the OpenNN library.
-/// A neural network here is defined as a multilayer perceptron extended with a scaling layer, an unscaling layer, 
-/// a bounding layer and a probabilistic layer.
-/// This neural network is used to span a function space for the variational problem at hand. 
+
+///
+/// This neural network is used to span a function space for the problem at hand.
 
 class NeuralNetwork
 {
 
 public:
 
-   // DEFAULT CONSTRUCTOR
+    enum ProjectType{Approximation, Classification, Forecasting, ImageApproximation, ImageClassification};
+
+   // Constructors
 
    explicit NeuralNetwork();
 
-   // MULTILAYER PERCEPTRON CONSTRUCTOR
+   explicit NeuralNetwork(const NeuralNetwork::ProjectType&, const Vector<size_t>&);
 
-   explicit NeuralNetwork(const MultilayerPerceptron& new_multilayer_perceptron);
+   explicit NeuralNetwork(const Vector<size_t>&, const size_t&, const Vector<size_t>&, const size_t&);
 
-   // MULTILAYER PERCEPTRON ARCHITECTURE CONSTRUCTOR
+   explicit NeuralNetwork(const string&);
 
-   explicit NeuralNetwork(const Vector<size_t>& new_multilayer_perceptron_architecture);
+   explicit NeuralNetwork(const tinyxml2::XMLDocument&);
 
-   explicit NeuralNetwork(const vector<size_t>& new_multilayer_perceptron_architecture);
+   explicit NeuralNetwork(const Vector<Layer*>&);
 
+   NeuralNetwork(const NeuralNetwork&);
 
-    // ONE PERCEPTRON LAYER CONSTRUCTOR
-
-   explicit NeuralNetwork(const size_t& new_inputs_number, const size_t& new_outputs_number);
-
-   // TWO PERCEPTRON LAYERS CONSTRUCTOR
-
-   explicit NeuralNetwork(const size_t& new_inputs_number, const size_t& new_hidden_perceptrons_number, const size_t& new_outputs_number);
-
-   // FILE CONSTRUCTOR
-
-   explicit NeuralNetwork(const string& file_name);
-
-   // XML CONSTRUCTOR
-
-   explicit NeuralNetwork(const tinyxml2::XMLDocument& document);
-
-   // COPY CONSTRUCTOR
-
-   NeuralNetwork(const NeuralNetwork& other_neural_network);
-
-   // DESTRUCTOR
+   // Destructor
 
    virtual ~NeuralNetwork();
 
-   // ASSIGNMENT OPERATOR
+   // APPENDING LAYERS
 
-   NeuralNetwork& operator = (const NeuralNetwork& other_neural_network);
+   void add_layer(Layer*);
 
-   // EQUAL TO OPERATOR
+   bool check_layer_type(const Layer::LayerType);
 
-   bool operator == (const NeuralNetwork& other_neural_network) const;
+   // Get methods
 
-
-   // GET METHODS
-
-   bool has_multilayer_perceptron() const;
-   bool has_inputs() const;
-   bool has_outputs() const;
-   bool has_inputs_trending_layer() const;
    bool has_scaling_layer() const;
    bool has_principal_components_layer() const;
+   bool has_long_short_term_memory_layer() const;
+   bool has_recurrent_layer() const;
    bool has_unscaling_layer() const;
-   bool has_outputs_trending_layer() const;
    bool has_bounding_layer() const;
    bool has_probabilistic_layer() const;
 
-   MultilayerPerceptron* get_multilayer_perceptron_pointer() const;
-   Inputs* get_inputs_pointer() const;
-   Outputs* get_outputs_pointer() const;
-   InputsTrendingLayer* get_inputs_trending_layer_pointer() const;
+   bool is_empty() const;  
+
+   Vector<string> get_inputs_names() const;
+   string get_input_name(const size_t&) const;
+   size_t get_input_index(const string&) const;
+
+   Vector<string> get_outputs_names() const;
+   string get_output_name(const size_t&) const;
+   size_t get_output_index(const string&) const;
+
+   Vector<Layer*> get_layers_pointers() const;
+   Vector<Layer*> get_trainable_layers_pointers() const;
+   Vector<size_t> get_trainable_layers_indices() const;
+
    ScalingLayer* get_scaling_layer_pointer() const;
-   PrincipalComponentsLayer* get_principal_components_layer_pointer() const;
-   UnscalingLayer* get_unscaling_layer_pointer() const;   
-   OutputsTrendingLayer* get_outputs_trending_layer_pointer() const;
+   UnscalingLayer* get_unscaling_layer_pointer() const;
    BoundingLayer* get_bounding_layer_pointer() const;
    ProbabilisticLayer* get_probabilistic_layer_pointer() const;
+   PrincipalComponentsLayer* get_principal_components_layer_pointer() const;
+   LongShortTermMemoryLayer* get_long_short_term_memory_layer_pointer() const;
+   RecurrentLayer* get_recurrent_layer_pointer() const;
+
+   Layer* get_output_layer_pointer() const;
+   Layer* get_layer_pointer(const size_t&) const;
+   PerceptronLayer* get_first_perceptron_layer_pointer() const;
 
    const bool& get_display() const;
 
-   // SET METHODS
+   // Set methods
 
    void set();
 
-   void set(const MultilayerPerceptron& new_multilayer_perceptron);
-   void set(const Vector<size_t>& new_multilayer_perceptron_architecture);
-   void set(const size_t& new_inputs_number, const size_t& new_outputs_number);
-   void set(const size_t& new_inputs_number, const size_t& new_hidden_neurons_number, const size_t& new_outputs_number);
+   void set(const NeuralNetwork::ProjectType&, const Vector<size_t>&);
+   void set(const Vector<size_t>&, const size_t&, const Vector<size_t>&, const size_t&);
 
-   void set(const size_t&);
+   void set(const string&);
+   void set(const NeuralNetwork&);
 
-   void set(const string& file_name);
-   void set(const NeuralNetwork& other_neural_network);
+   void set_inputs_names(const Vector<string>&);
+   void set_outputs_names(const Vector<string>&);
 
-   void set_inputs(const Vector<bool>& new_uses);
+   void set_inputs_number(const size_t&);
+   void set_inputs_number(const Vector<bool>&);
 
    virtual void set_default();
 
-#ifdef __OPENNN_MPI__
-   void set_MPI(const NeuralNetwork* neural_network);
-#endif
-
-   void set_multilayer_perceptron_pointer(MultilayerPerceptron*);
-   void set_inputs_trending_layer_pointer(InputsTrendingLayer*);
-   void set_scaling_layer_pointer(ScalingLayer*);
-   void set_principal_components_layer_pointer(PrincipalComponentsLayer*);
-   void set_unscaling_layer_pointer(UnscalingLayer*);
-   void set_outputs_trending_layer_pointer(OutputsTrendingLayer*);
-   void set_bounding_layer_pointer(BoundingLayer*);
-   void set_probabilistic_layer_pointer(ProbabilisticLayer*);
-   void set_inputs_pointer(Inputs*);
-   void set_outputs_pointer(Outputs*);
+   void set_layers_pointers(Vector<Layer*>&);
 
    void set_scaling_layer(ScalingLayer&);
 
    void set_display(const bool&);
 
-   // Growing and pruning
-
-   void grow_input(const Statistics<double>& new_statistics = Statistics<double>());
-
-   void prune_input(const size_t& index);
-   void prune_output(const size_t& index);
-
-   void resize_inputs_number(const size_t&);
-   void resize_outputs_number(const size_t&);
-
-   // Pointer methods
-
-   void construct_multilayer_perceptron();
-   void construct_inputs_trending_layer();
-   void construct_scaling_layer();
-   void construct_scaling_layer(const Vector< Statistics<double> >& input_statistics);
-   void construct_scaling_layer(const Eigen::MatrixXd& input_statistics);
-   void construct_principal_components_layer();
-   void construct_unscaling_layer();
-   void construct_unscaling_layer(const Vector< Statistics<double> >& target_statistics);
-   void construct_unscaling_layer(const Eigen::MatrixXd& target_statistics);
-   void construct_outputs_trending_layer();
-   void construct_bounding_layer();
-   void construct_probabilistic_layer();
-   void construct_inputs();
-   void construct_outputs();
-
-   void destruct_multilayer_perceptron();
-   void destruct_inputs_trending_layer();
-   void destruct_scaling_layer();
-   void destruct_unscaling_layer();
-   void destruct_outputs_trending_layer();
-   void destruct_bounding_layer();
-   void destruct_probabilistic_layer();
-   void destruct_inputs();
-   void destruct_outputs();
-
-   void delete_pointers();
-
-   // Initialization methods
-
-   void initialize_random();
-
    // Layers 
 
    size_t get_layers_number() const;
+   Vector<size_t> get_layers_neurons_numbers() const;
+
+   size_t get_trainable_layers_number() const;
 
    // Architecture
 
@@ -231,73 +160,68 @@ public:
    // Parameters
 
    size_t get_parameters_number() const;
+   size_t get_trainable_parameters_number() const;
    Vector<double> get_parameters() const;
 
-   void set_parameters(const Vector<double>& new_parameters);
+   Vector<size_t> get_trainable_layers_parameters_numbers() const;
+
+   Vector<Vector<double>> get_trainable_layers_parameters(const Vector<double>&) const;
+
+   void set_parameters(const Vector<double>&);
 
    // Parameters initialization methods
 
-   void initialize_parameters(const double& value);
+   void initialize_parameters(const double&);
 
-   void randomize_parameters_uniform();
-   void randomize_parameters_uniform(const double& minimum, const double& maximum);
-   void randomize_parameters_uniform(const Vector<double>& minimum, const Vector<double>& maximum);
-   void randomize_parameters_uniform(const Vector< Vector<double> >& minimum_maximum);
+   void randomize_parameters_uniform(const double& = -1.0, const double& = 1.0);  
 
-   void randomize_parameters_normal();
-   void randomize_parameters_normal(const double& mean, const double& standard_deviation);
-   void randomize_parameters_normal(const Vector<double>& mean, const Vector<double>& standard_deviation);
-   void randomize_parameters_normal(const Vector< Vector<double> >& mean_standard_deviation);
+   void randomize_parameters_normal(const double& = 0.0, const double& = 1.0);  
 
    // Parameters
 
    double calculate_parameters_norm() const;
-   Statistics<double> calculate_parameters_statistics() const;
-   Histogram<double> calculate_parameters_histogram(const size_t& bins_number = 10) const;
+   Descriptives calculate_parameters_descriptives() const;
+   Histogram calculate_parameters_histogram(const size_t& = 10) const;
 
    void perturbate_parameters(const double&);
 
-   // Feature importance
-
-   Vector<double> calculate_inputs_importance_parameters(const size_t& output_index) const;
-
    // Output 
 
-   Matrix<double> calculate_outputs(const Matrix<double>& inputs) const;
-   Matrix<double> calculate_outputs(const Matrix<double>& inputs, const double& time) const;
-   Eigen::MatrixXd calculate_outputs_eigen(const Eigen::MatrixXd& inputs) const;
-   Vector< Matrix<double> > calculate_Jacobian(const Matrix<double>& inputs) const;
-   Matrix<double> calculate_Jacobian(const Vector<double>& inputs, const double& time) const;
-   Vector< Matrix<double> > calculate_Hessian(const Vector<double>& inputs) const;
+   Tensor<double> calculate_outputs(const Tensor<double>&);
+   Eigen::MatrixXd calculate_outputs_eigen(const Eigen::MatrixXd&);
 
-   Matrix<double> calculate_directional_inputs(const size_t& direction, const Vector<double>& point, const double& minimum, const double& maximum, const size_t& points_number = 101) const;
+   Tensor<double> calculate_trainable_outputs(const Tensor<double>&) const;
 
-   Vector< Matrix<double> > calculate_Jacobian_data(const Matrix<double>& inputs) const;
+   Tensor<double> calculate_trainable_outputs(const Tensor<double>&, const Vector<double>&) const;
 
-   Vector< Histogram<double> > calculate_outputs_histograms(const size_t& points_number = 1000, const size_t& bins_number = 10) const;
-   Vector< Histogram<double> > calculate_outputs_histograms(const Matrix<double>& inputs, const size_t& bins_number = 10) const;
+   Matrix<double> calculate_directional_inputs(const size_t&, const Vector<double>&, const double&, const double&, const size_t& = 101) const;
 
+   Vector<Histogram> calculate_outputs_histograms(const size_t& = 1000, const size_t& = 10);
+   Vector<Histogram> calculate_outputs_histograms(const Tensor<double>&, const size_t& = 10);
 
-   vector<double> calculate_outputs_std(const vector<double>& inputs) const;
+   vector<double> calculate_outputs_std(const vector<double>&);
 
    // Serialization methods
 
    string object_to_string() const;
  
+   Matrix<string> get_information() const;
+
    virtual tinyxml2::XMLDocument* to_XML() const;
-   virtual void from_XML(const tinyxml2::XMLDocument& document);
+   virtual void from_XML(const tinyxml2::XMLDocument&);
 
    virtual void write_XML(tinyxml2::XMLPrinter&) const;
-   // virtual void read_XML(   );
+   // virtual void read_XML(  );
 
    void print() const;
-   void save(const string& file_name) const;
-   void save_parameters(const string& file_name) const;
+   void print_summary() const;
+   void save(const string&) const;
+   void save_parameters(const string&) const;
 
-   virtual void load(const string& file_name);
-   void load_parameters(const string& file_name);
+   virtual void load(const string&);
+   void load_parameters(const string&);
 
-   void save_data(const string& file_name) const;
+   void save_data(const string&) const;
 
    // Expression methods
 
@@ -307,64 +231,31 @@ public:
    string write_expression_php() const;
    string write_expression_R() const;
 
-   void save_expression(const string& file_name);
-   void save_expression_python(const string& file_name);
-   void save_expression_R(const string& file_name);
+   void save_expression(const string&);
+   void save_expression_python(const string&);
+   void save_expression_R(const string&);
 
-   // PMML methods
+   /// Calculate de Forward Propagation in Neural Network   
 
-   tinyxml2::XMLDocument* to_PMML() const;
-   void write_PMML(const string& file_name) const;
-
-   void from_PMML(const tinyxml2::XMLDocument& document);
+   Vector<Layer::FirstOrderActivations> calculate_trainable_forward_propagation(const Tensor<double>&) const;
 
 protected:
 
-   // MEMBERS
+   /// Names of inputs
 
-   /// Pointer to a multilayer perceptron object.
+   Vector<string> inputs_names;
 
-   MultilayerPerceptron* multilayer_perceptron_pointer = nullptr;
+   /// Names of ouputs
 
-   /// Pointer to an inputs trending layer object.
+   Vector<string> outputs_names;
 
-   InputsTrendingLayer* inputs_trending_layer_pointer = nullptr;
+   /// Layers
 
-   /// Pointer to a scaling layer object.
+   Vector<Layer*> layers_pointers;
 
-   ScalingLayer* scaling_layer_pointer = nullptr;
+   /// Display messages to screen.
 
-   /// Pointer to a principal components layer object.
-
-   PrincipalComponentsLayer* principal_components_layer_pointer = nullptr;
-
-   /// Pointer to an unscaling layer object.
-
-   UnscalingLayer* unscaling_layer_pointer = nullptr;
-
-   /// Pointer to an outputs trending layer object.
-
-   OutputsTrendingLayer* outputs_trending_layer_pointer = nullptr;
-
-   /// Pointer to a bounding layer object.
-
-   BoundingLayer* bounding_layer_pointer = nullptr;
-
-   /// Pointer to a probabilistic layer.
-
-   ProbabilisticLayer* probabilistic_layer_pointer = nullptr;
-
-   /// Pointer to an inputs object.
-
-   Inputs* inputs_pointer = nullptr;
-
-   /// Pointer to an outputs object.
-
-   Outputs* outputs_pointer = nullptr;
-
-   /// Display messages to screen. 
-
-   bool display;
+   bool display = true;
 };
 
 }
@@ -389,4 +280,3 @@ protected:
 // License along with this library; if not, write to the Free Software
 
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
